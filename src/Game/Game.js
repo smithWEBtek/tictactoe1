6a57cd669f773562fcad163257a298ab0ff2399b
github username <github email address>
2020-04-02 22:41:23 -0400

    Add more

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import Board from '../Board/Board';
 
 class Game extends Component {
+  state = {
+    currentPlayer: 'X',
+    turnCount: 0,
+    status: 'Game on'
+  }
+
   render() {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            className="Board"
+            gameStatus={this.props.status} />
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+        <div className="game-info">Current Player: {this.state.currentPlayer}
+          <ol>{this.state.games}</ol>
         </div>
       </div>
     );
